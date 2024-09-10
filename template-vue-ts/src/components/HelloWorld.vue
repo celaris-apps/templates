88d5d07d52fe87ee95ddcd811d6ccffb5d9b3d0c
Graham <gcoulby@googlemail.com>
2024-09-10 16:45:01 +0100

    updated vue-ts

--- template-vue-ts/src/components/HelloWorld.vue
+++ template-vue-ts/src/components/HelloWorld.vue
@@ -3,35 +3,26 @@ import { ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const greetMsg = ref('')
+const name = ref('')
+
+async function greet() {
+  window.greet(name.value).then((res) => {
+    //@ts-ignore
+    greetMsg.value = res['response']
+  })
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+  <form class="row" @submit.prevent="greet">
+    <input id="greet-input" v-model="name" placeholder="Enter a name..." />
+    <button type="submit">Greet</button>
+  </form>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p>{{ greetMsg }}</p>
 </template>
 
 <style scoped>
