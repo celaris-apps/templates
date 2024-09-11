e736385de89f1e9e2f93a690a6e17dde3545e4cb
Graham <gcoulby@googlemail.com>
2024-09-11 19:35:36 +0100

    updated react-ts

--- template-react-ts/src/App.tsx
+++ template-react-ts/src/App.tsx
@@ -1,33 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import celarisLogo from './assets/celaris.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+  const [greetMsg, setGreetMsg] = useState('')
+
+  async function greet() {
+    window.greet(name).then((res) => {
+      setGreetMsg(res.message)
+    })
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://celaris.cc/" target="_blank">
+          <img src={celarisLogo} class="logo celaris" alt="Celaris logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Celaris</h1>
+
+      <input id="greet-input" value={name} onInput={(e) => setName(e.target.value)} placeholder="Enter a name..." />
+      <button type="submit" onClick={() => greet}>
+        Greet
+      </button>
+      <p>{greetMsg}</p>
     </>
   )
 }
